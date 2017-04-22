94b2df523bbb17c9aec0e36eccb14edefe0007fb
xin <503650594@qq.com>
2017-04-22 18:07:27 +0800

    add biliresample

--- resampling.cpp
+++ resampling.cpp
@@ -4,6 +4,10 @@ void resample(const Mat &origin, Mat &target, int trows, int tcols, int choice)
 {
 	if(choice == 0)
 		nnresample(origin, target, trows, tcols);
+	else if(choice == 1)
+		biliresample(origin, target, trows, tcols);
+	else if(choice == 2)
+		bicubicresample(origin, target, trows, tcols);
 }
 
 void nnresample(const Mat &origin, Mat &target, int trows, int tcols)
@@ -34,4 +38,37 @@ void nnresample(const Mat &origin, Mat &target, int trows, int tcols)
 
 			target.at<Vec3b>(row, col) = origin.at<Vec3b>(round(rf), round(cf));
 		}
+}
+
+void biliresample(const Mat &origin, Mat &target, int trows, int tcols)
+{
+	int rows = origin.rows;
+	int cols = origin.cols;
+
+	//scale coefficient
+	double sr,sc;
+	sr = double(rows) / trows;
+	sc = double(cols) / tcols;
+
+	target = Mat(trows, tcols, CV_8UC3);
+
+	for(int row = 0; row < trows; row++)
+		for(int col = 0; col < tcols; col++)
+		{
+			double rf = sr * row;
+			double cf = sc * col;
+
+			int r = floor(rf);
+			int c = floor(cf);
+
+			double dr = rf - r;
+			double dc = cf - c;
+
+			target.at<Vec3b>(row, col) = origin.at<Vec3b>(r, c) * (1 - dr) * (1 - dc) + origin.at<Vec3b>(r + 1, c) * dr * (1 - dc) + origin.at<Vec3b>(r, c + 1) * (1 - dr) * dc + origin.at<Vec3b>(r + 1 , c + 1) * dr * dc; 
+		}
+}
+
+void bicubicresample(const Mat &origin, Mat &target, int trows, int tcols)
+{
+
 }
--- resampling.h
+++ resampling.h
@@ -4,3 +4,7 @@ using namespace cv;
 void resample(const Mat &origin, Mat &target, int trows, int tcols, int choice);
 
 void nnresample(const Mat &origin, Mat &target, int trows, int tcols);
+
+void biliresample(const Mat &origin, Mat &target, int trows, int tcols);
+
+void bicubicresample(const Mat &origin, Mat &target, int trows, int tcols);
